aff4cdf87cf418ca58544e0dc7d8b7e890f3fe15
Akash Patil <akashivu002@gmail.com>
2025-11-01 16:26:57 +0530

    resolved TypeScript errors for Google Maps integration

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,25 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'gmpx-api-loader': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      key?: string;
-      'solution-channel'?: string;
-    };
-    'gmpx-place-autocomplete': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      ref?: React.Ref<any>;
-    };
+/// <reference types="google.maps" />
+export {};
+
+declare global {
+  interface Window {
+    google: typeof google;
   }
 }
+
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'gmpx-api-loader': React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    > & { 'solution-channel'?: string };
+    'gmpx-place-autocomplete': React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    > & { ref?: React.Ref<any> };
+  }
+}
+
+
 declare module "swiper/css";
